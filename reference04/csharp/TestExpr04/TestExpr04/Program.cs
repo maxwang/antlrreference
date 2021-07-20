91c6a56eb8c1727c4307a28f566f04f9374b05c2
Max <max@adaxonline.com>
2021-07-20 23:57:14 +1000

    add console

---
+++ reference04/csharp/TestExpr04/TestExpr04/Program.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using System;
+
+namespace TestExpr04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "a=1";
+            var inputStream = new AntlrInputStream(input);
+            ExprLexer lexer = new ExprLexer(inputStream);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new ExprParser(tokens);
+            var tree = parser.prog();
+            Console.WriteLine(tree.ToStringTree());
+            Console.ReadLine();
+        }
+    }
+}
